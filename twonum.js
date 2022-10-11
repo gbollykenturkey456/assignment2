1350b58a18774cd1ef96159cee4809b5d65f5b34
Gbolly <gbolahanakins7@gmail.com>
2022-10-11 18:11:56 +0100

    my two integer to commit

---
+++ twonum.js
@@ -0,0 +1,23 @@
+const Numbers = alert("The sum of two given Integers");
+
+
+function twoNumbers (n1, n2) { 
+    
+    if (n1 == n2) {
+        
+        return 3 * (n1 + n2);
+    }
+
+    else {
+        
+        return (n1 + n2);
+
+    }
+    
+}
+
+document.write  (twoNumbers (-20,8));
+
+document.write  ("<br>" + twoNumbers (15,15));
+
+
